f417db81769e2ae401a5a06a7595d7ad880842e2
maxxgta <maxxgta@hotmail.com>
2015-11-23 17:32:54 -0500

    Import 2

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,24 @@
 function butOnClick(target){
  iteNumeros = document.getElementById('iteNumeros');
  if (target.id == "but0"){
- iteNumeros.value = 0++;
+ iteNumeros.value = 0;
  }else if (target.id == "but1"){
- iteNumeros.value = 1++;
+ iteNumeros.value = 1;
  }else if (target.id == "but2"){
- iteNumeros.value = 2++;
+ iteNumeros.value = 2;
  }else if (target.id == "but3"){
- iteNumeros.value = 3++;
+ iteNumeros.value = 3;
  }else if (target.id == "but4"){
- iteNumeros.value = 4++;
+ iteNumeros.value = 4;
  }else if (target.id == "but5"){
- iteNumeros.value = 5++;
+ iteNumeros.value = 5;
  }else if (target.id == "but6"){
- iteNumeros.value = 6++;
+ iteNumeros.value = 6;
  }else if (target.id == "but7"){
- iteNumeros.value = 7++;
+ iteNumeros.value = 7;
  }else if (target.id == "but8"){
- iteNumeros.value = 8++;
+ iteNumeros.value = 8;
  }else if (target.id == "but9"){
- iteNumeros.value = 9++;
+ iteNumeros.value = 9;
  }
 }
