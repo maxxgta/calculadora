b9b7803c685767e853fef22cd4002953ce3ddd13
maxxgta <maxxgta@hotmail.com>
2015-11-24 15:18:13 -0500

    final import

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,52 @@
+var aux=0;
+var op="";
 function butOnClick(target){
- iteNumeros = document.getElementById('iteNumeros');
+iteNumeros = document.getElementById('iteNumeros');
  if (target.id == "but0"){
- iteNumeros.value = 0;
+ iteNumeros.value = iteNumeros.value+0;
  }else if (target.id == "but1"){
- iteNumeros.value = 1;
+ iteNumeros.value = iteNumeros.value+1;
  }else if (target.id == "but2"){
- iteNumeros.value = 2;
+ iteNumeros.value = iteNumeros.value+2;
  }else if (target.id == "but3"){
- iteNumeros.value = 3;
+ iteNumeros.value = iteNumeros.value+3;
  }else if (target.id == "but4"){
- iteNumeros.value = 4;
+ iteNumeros.value = iteNumeros.value+4;
  }else if (target.id == "but5"){
- iteNumeros.value = 5;
+ iteNumeros.value = iteNumeros.value+5;
  }else if (target.id == "but6"){
- iteNumeros.value = 6;
+ iteNumeros.value = iteNumeros.value+6;
  }else if (target.id == "but7"){
- iteNumeros.value = 7;
+ iteNumeros.value = iteNumeros.value+7;
  }else if (target.id == "but8"){
- iteNumeros.value = 8;
+ iteNumeros.value = iteNumeros.value+8;
  }else if (target.id == "but9"){
- iteNumeros.value = 9;
- }
+ iteNumeros.value = iteNumeros.value+9;
+ }else if (target.id == "butMas"){
+ aux=parseInt(iteNumeros.value);
+ iteNumeros.value=""; 
+ op="suma";   
+ }else if (target.id == "butMenos"){
+ aux=parseInt(iteNumeros.value);
+ iteNumeros.value="";
+ op="resta";
+ }else if (target.id == "butPor"){
+ aux=parseInt(iteNumeros.value);
+ iteNumeros.value="";
+ op="multi";
+ }else if (target.id == "butEntre"){
+ aux=parseInt(iteNumeros.value);
+ iteNumeros.value="";
+ op="div";
+ }else if (target.id == "butIgual"){  
+  if(op == "suma"){
+  iteNumeros.value = String(aux + parseInt(iteNumeros.value));
+  }else if (op == "resta"){
+  iteNumeros.value = String(aux - parseInt(iteNumeros.value));
+  }else if (op == "multi"){
+  iteNumeros.value = String(aux * parseInt(iteNumeros.value));
+  }else if (op == "div"){
+  iteNumeros.value = String(aux / parseInt(iteNumeros.value));
+  }
+ } 
 }
